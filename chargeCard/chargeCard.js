4a832a57c3621ef6aa85d8e9976d219f08370a02
dantehsema@yahoo.com <dantehsema@yahoo.com>
2019-07-17 16:12:40 -0400

    started chargeCard question

---
+++ chargeCard/chargeCard.js
@@ -0,0 +1,24 @@
+/*
+
+Create a cash register class, a seller class and a buyer class
+
+Buyer 
+Properties 
+- has a cash reserve
+- buy list 
+
+Methods
+- Start payment 
+
+Cash Register 
+Properties
+
+Methods
+
+Seller 
+Properties
+- 
+
+Methods
+
+*/
