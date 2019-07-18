394dd2d2d30c546fc3781a3fd3fb0710e4b83e1a
dantehsema@yahoo.com <dantehsema@yahoo.com>
2019-07-17 20:10:50 -0400

    updated question

--- popularSongs/popularSongs.js
+++ popularSongs/popularSongs.js
@@ -1,19 +1,15 @@
 /*
 
+Part I 
 create a music player app
 
 your app should display a list of artist that have albums 
 
-Your app should hold a list of albums
+your app should hold a list of albums for each artist
 
 your app should display all the songs for one album 
 
 you app should keep track of how many times a song is played
-
-your app should have a play song function that turn 
-
-your app should display the most popular songs from an artist discography list 
-
 
 
 create an album class 
@@ -35,9 +31,11 @@ increases the 'plays' properties by +1 on the songs class
 
 BONUS 
 Your app should account for artist that only have individual songs
-switches playSong boolean on or off 
+your app has playSong boolean that turns a song on or off 
 
 
+Part II
+your app should display the most popular songs from an artist discography list 
 */
 
 
