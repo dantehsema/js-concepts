594a458e4eb46474a7d3b4775a9d0d1d01ab205d
dantehsema@yahoo.com <dantehsema@yahoo.com>
2019-07-17 20:22:56 -0400

    updated human logic

--- popularSongs/popularSongs.js
+++ popularSongs/popularSongs.js
@@ -10,21 +10,46 @@ your app should hold a list of albums for each artist
 your app should display all the songs for one album 
 
 you app should keep track of how many times a song is played
+\\\\\\\\\\\\\\\\\\\\\\\\\\\
+create an app class 
+properties 
+artistList
 
+methods
+add artist
+\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
+\\\\\\\\\\\\\\\\\\\\\\\\\\\
+create an artist class 
+properties
+albumList: [Albums, Albums, Albums]
+name: string;
+
+methods
+add albums
+\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+\\\\\\\\\\\\\\\\\\\\\\\\\\\
 create an album class 
 properties 
 title : string;
 artist : string;
-songs : [Song, Song, Song];
+songsList : [Song, Song, Song];
 
+method
+add song
+\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+\\\\\\\\\\\\\\\\\\\\\\\\\\\
 Create a Song class 
 properties
 title : string;
 artist : string;
 plays : number;
-playSong: boolean;
 
+method
+play song
+\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 Play Song function 
 increases the 'plays' properties by +1 on the songs class
@@ -39,80 +64,80 @@ your app should display the most popular songs from an artist discography list
 */
 
 
-class App {
-	artistList = [];
+// class App {
+// 	artistList = [];
 
-	constructor() {
+// 	constructor() {
 
-	}
+// 	}
 
-	addArtist (artist) {
-		return this.artistList.push(artist);
-	}
+// 	addArtist (artist) {
+// 		return this.artistList.push(artist);
+// 	}
 
-}
+// }
 
-class Artist {
+// class Artist {
 	
-	albumList = [];
+// 	albumList = [];
 
-	constructor(name) {
-		this.name = name;
-	}
+// 	constructor(name) {
+// 		this.name = name;
+// 	}
 
-	addAlbum (album) {
-		this.albumList.push(album);
-	}
+// 	addAlbum (album) {
+// 		this.albumList.push(album);
+// 	}
 
-}
+// }
 
-class Song {
-	plays = 0; 
+// class Song {
+// 	plays = 0; 
 
-	constructor(artist, title) {
-		this.title = title; 
-		this.artist = artist;
-	}
+// 	constructor(artist, title) {
+// 		this.title = title; 
+// 		this.artist = artist;
+// 	}
 
-	songPlayed() {
-		this.plays +=1;
-		console.log('the song', this)
-		return this;
-	}
+// 	songPlayed() {
+// 		this.plays +=1;
+// 		console.log('the song', this)
+// 		return this;
+// 	}
 
-}
+// }
 
-class Album {
-	songList = [];
-	newSong; 
-	constructor(artist, title) {
-		this.title = title;
-		this.artist = artist;
-	}
+// class Album {
+// 	songList = [];
+// 	newSong; 
+// 	constructor(artist, title) {
+// 		this.title = title;
+// 		this.artist = artist;
+// 	}
 
 
-	addSong (artist, title) {
-		this.newSong = new Song(artist, title);
-		console.log('the new song', this.newSong)
-		this.songList.push(this.newSong);
-	}
+// 	addSong (artist, title) {
+// 		this.newSong = new Song(artist, title);
+// 		console.log('the new song', this.newSong)
+// 		this.songList.push(this.newSong);
+// 	}
 
-}
+// }
 
-let newApp = new App();
+// let newApp = new App();
 
-let album1 = new Album('carine', 'got to be great');
-album1.addSong('carine', 'she\'s the greatest')
-album1.addSong('carine', 'two can play that game');
+// let album1 = new Album('carine', 'got to be great');
+// album1.addSong('carine', 'she\'s the greatest')
+// album1.addSong('carine', 'two can play that game');
 
-let newArtist = new Artist('carine');
-newArtist.addAlbum(album1);
+// let newArtist = new Artist('carine');
+// newArtist.addAlbum(album1);
 
-newApp.addArtist(newArtist); 
+// newApp.addArtist(newArtist); 
 
-album1.songList[0].songPlayed();
+// album1.songList[0].songPlayed();
 
-console.log(`this is the ${newArtist.name} discography now`, newArtist)
+// console.log(`this is the ${newArtist.name} discography now`, newArtist)
 
 
 
